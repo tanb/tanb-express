d2b63d4570f0acb503c96323dbddc13c357fb254
tanb <tanb@me.com>
2018-09-20 14:36:07 +0900

    Use animejs

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,12 +1,10 @@
 import { Component, ComponentRef, OnInit, AfterViewInit } from '@angular/core';
-import { trigger, state, style, animate, transition } from '@angular/animations';
 
 import * as moment from 'moment';
 import * as anime from 'animejs';
 
 import { ContactMeComponent } from 'src/app/core/modal/contact-me/contact-me.component';
 import { ModalService } from 'src/app/core/services/modal.service';
-
 
 enum BalloonState {
   top = 'top',
@@ -17,20 +15,6 @@ enum BalloonState {
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.scss'],
-  animations: [
-    trigger('balloon', [
-      state(BalloonState.top, style({
-        transform: 'translateY(-20px)'
-      })),
-      state(BalloonState.bottom, style({
-        transform: 'translateY(0px)'
-      })),
-      transition(BalloonState.bottom + ' => ' + BalloonState.top,
-                 animate('7000ms ease-in-out')),
-      transition(BalloonState.top + ' => ' + BalloonState.bottom,
-                 animate('7000ms ease-in-out'))
-    ]),
-  ]
 })
 export class ProfileComponent implements OnInit, AfterViewInit {
   balloonState: BalloonState = BalloonState.bottom;
@@ -53,23 +37,18 @@ export class ProfileComponent implements OnInit, AfterViewInit {
       age: myAge,
       round: 1,
       easing: 'linear',
-      update: (function(a) {
+      update: (function() {
         this.age = targets.age;
       }).bind(this);
     });
-  }
-
-  onEnd(event) {
-    const wait = (Math.floor(Math.random() * 9) + 1) * 100;
-    if (event.toState === BalloonState.bottom) {
-      setTimeout(() => {
-        this.balloonState = BalloonState.top;
-      }, wait);
-    } else {
-      setTimeout(() => {
-        this.balloonState = BalloonState.bottom;
-      }, wait);
-    }
+    anime({
+      targets: '.tanb-icon .rounded-circle',
+      translateY: -33,
+      duration: 8000,
+      easing: 'easeInOutSine',
+      direction: 'alternate',
+      loop: true
+    });
   }
 
   openModal() {

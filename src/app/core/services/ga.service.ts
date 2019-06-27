dc61dd9d6c9782269b8959fb962584bf72525b12
tanb <tanb@me.com>
2019-06-28 01:35:10 +0900

    Fix settings

--- src/app/core/services/ga.service.ts
+++ src/app/core/services/ga.service.ts
@@ -27,21 +27,14 @@ export class GaService {
   }
 
   get enabled(): boolean {
-    return this.cookies[this.DISABLE_GA_KEY] === undefined;
+    return localStorage.getItem(this.DISABLE_GA_KEY) !== 'disabled';
   }
 
   set enabled(enabled: boolean) {
-    // Remove disable-key if enabled;
-    let maxAge = 0;
-    let expireDate = new Date(0);
-    let value = '';
-    if (!enabled) {
-      // Set disable-key yes if disabled. (4 weeks)
-      maxAge = 60 * 60 * 24 * 7 * 4;
-      expireDate = new Date();
-      expireDate.setSeconds(expireDate.getSeconds() + maxAge);
-      value = 'yes';
+    if (enabled) {
+      localStorage.removeItem(this.DISABLE_GA_KEY);
+    } else {
+      localStorage.setItem(this.DISABLE_GA_KEY, 'disabled');
     }
-    document.cookie = `${this.DISABLE_GA_KEY}=${value}; expires=${expireDate.toUTCString()}; max-age=${maxAge}; `;
   }
 }

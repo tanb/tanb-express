7e2ac8ec4b91dd8f10dbfeed8dbeca0f7e9f9a15
tanb <tanb@me.com>
2019-09-13 05:26:47 +0900

    Adding dark-mode style

--- src/app/header-title/header-title.component.ts
+++ src/app/header-title/header-title.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Input, Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header-title',
@@ -6,10 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header-title.component.scss']
 })
 export class HeaderTitleComponent implements OnInit {
-
+  @Input() layout: 'mobile' | 'desktop';
   constructor() { }
 
   ngOnInit() {
   }
-
 }

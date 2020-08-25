f31500f72573ddf698c6cd4a294bb8a9f938fc77
tanb <tanb@me.com>
2020-08-25 16:41:27 +0900

    Add sitemap generator. Fix path. Remove unused files.

--- scully.tanb-express.config.ts
+++ scully.tanb-express.config.ts
@@ -1,4 +1,34 @@
-import { ScullyConfig } from '@scullyio/scully';
+import { ScullyConfig, setPluginConfig } from '@scullyio/scully';
+import { getSitemapPlugin } from '@gammastream/scully-plugin-sitemap';
+
+const SitemapPlugin = getSitemapPlugin();
+setPluginConfig(SitemapPlugin, {
+  urlPrefix: 'https://tanb.express',
+  sitemapFilename: 'sitemap.xml',
+  changeFreq: 'monthly',
+  priority: [
+    '1.0',
+    '0.9',
+    '0.8',
+    '0.7',
+    '0.6',
+    '0.5',
+    '0.4',
+    '0.3',
+    '0.2',
+    '0.1',
+    '0.0',
+  ],
+  ignoredRoutes: ['/404', '/settings'],
+  routes: {
+    '/blog/:slug': {
+      changeFreq: 'daily',
+      priority: '0.9',
+      merge: true
+    },
+  },
+});
+
 export const config: ScullyConfig = {
   projectRoot: "./src",
   projectName: "tanb-express",

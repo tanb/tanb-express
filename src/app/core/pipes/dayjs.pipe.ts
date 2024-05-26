47ca501da2bd8937968bef1c21e560c0862c530d
Tomonori Tanabe <tanb@me.com>
2024-05-27 01:17:12 +0900

    fix

--- src/app/core/pipes/dayjs.pipe.ts
+++ src/app/core/pipes/dayjs.pipe.ts
@@ -2,16 +2,15 @@ import type { PipeTransform } from '@angular/core';
 import { Pipe } from '@angular/core';
 import dayjs from 'dayjs';
 
-
 dayjs.locale('ja');
 
 @Pipe({
   standalone: true,
-  name: 'dayjs'
+  name: 'dayjs',
 })
 export class DayjsPipe implements PipeTransform {
   transform(date: Date | string, format = 'YYYY/MM/DD HH:mm'): string {
-    let num = parseInt((date as string));
+    let num = parseInt(date as string);
     if (Number.isNaN(num)) {
       const d = dayjs(date);
       return d.format(format);
--- src/app/pages/tools/tools.component.ts
+++ src/app/pages/tools/tools.component.ts
@@ -1,20 +1,18 @@
 import type { OnInit } from '@angular/core';
 import { Component } from '@angular/core';
-import {FormsModule} from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+import { DayjsPipe } from '../../core/pipes/dayjs.pipe';
 
 @Component({
   standalone: true,
   selector: 'app-tools',
   templateUrl: './tools.component.html',
-  styleUrls: ['./tools.component.scss'],
-  imports: [
-    FormsModule
-  ]
+  imports: [FormsModule, DayjsPipe],
 })
 export class ToolsComponent implements OnInit {
   timestamp = '';
-  
+
   ngOnInit(): void {
-    this.timestamp = `${new Date().getTime()  }`;
+    this.timestamp = `${new Date().getTime()}`;
   }
 }
